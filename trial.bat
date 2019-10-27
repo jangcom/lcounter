72610512a503ae9d03f58828817f26e708f77a6f
jang <jangj@korea.ac.kr>
2019-10-27 11:45:34 +0900

    trial command added

--- trial.bat
+++ trial.bat
@@ -1,3 +1,9 @@
 @echo off
-perl lcounter.pl USAGE -path=./lines_counted -rpt=usage_counted.txt
+rem lcounter source code
 perl lcounter.pl lcounter.pl -path=./lines_counted -rpt=lcounter_counted.txt
+
+rem USAGE
+perl lcounter.pl USAGE -path=./lines_counted -rpt=usage_counted.txt
+
+rem lcounter source code + USAGE
+perl lcounter.pl lcounter.pl USAGE -path=./lines_counted -rpt=lcounter_usage_counted.txt
